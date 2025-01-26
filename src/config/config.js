c2ccd8363b713949104adc250c3fb1e2f7ad1db9
jjone161 <jordanjonesodu@gmail.com>
2025-01-26 01:44:56 -0500

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,16 @@
-export const config = {
-    apiUrl: 'https://l0tebdk8th.execute-api.us-east-1.amazonaws.com'
+import { Amplify } from 'aws-amplify';
+
+const config = {
+    API: {
+        endpoints: [
+            {
+                name: "francoGameApi",
+                endpoint: "https://l0tebdk8th.execute-api.us-east-1.amazonaws.com"
+            }
+        ]
+    }
 };
+
+Amplify.configure(config);
+
+export default config;
