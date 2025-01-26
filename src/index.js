dbd9b9115fc176d92cfefa881f172b9d4ac34aee
jjone161 <jordanjonesodu@gmail.com>
2025-01-25 22:00:18 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Amplify } from 'aws-amplify';
+import { amplifyConfig } from './config/amplify-config';
+import '@aws-amplify/ui-react/styles.css';
+
+// Configure Amplify
+Amplify.configure(amplifyConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
