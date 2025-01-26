baede67496ce7747e984cd218251d1330deb1d23
jjone161 <jordanjonesodu@gmail.com>
2025-01-25 22:06:16 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Amplify } from 'aws-amplify';
-import { amplifyConfig } from './config/amplify-config';
+import amplifyConfig from './config/amplify-config'; // Changed this line
 import '@aws-amplify/ui-react/styles.css';
 
 // Configure Amplify
