72763a978adace6661aaebee1e8a6bdb41ff0439
jjone161 <jordanjonesodu@gmail.com>
2025-01-27 15:51:00 -0500

    Delete amplify/backend/api/francoGameApi directory

--- amplify/backend/api/francoGameApi/override.ts
+++
@@ -1,9 +0,0 @@
-import { AmplifyApiGraphQlResourceStackTemplate } from '@aws-amplify/cli-extensibility-helper';
-
-export function override(resources: AmplifyApiGraphQlResourceStackTemplate) {
-  resources.restApi.addDependsOn({
-    category: 'function',
-    resourceName: 'franco-game-handler',
-    attributes: ['Name', 'Arn']
-  });
-}
